f23208cb52c18777d24209f35237db48a134bd46
sjcpu <sjhwang623@gmail.com>
2026-06-01 14:12:05 +0900

    Update sw.js

--- diet/sw.js
+++ diet/sw.js
@@ -1,4 +1,14 @@
-const CACHE = "diet-tracker-v1";
-const FILES = ["./", "./index.html"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'diet-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+ 
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+);
+self.addEventListener('activate', e =>
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ))
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
