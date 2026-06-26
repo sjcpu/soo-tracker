aeb165597d46187114183ca676b41a22256ca3fb
sjcpu <sjhwang623@gmail.com>
2026-06-26 14:32:20 +0900

    Update sw.js

--- diet/sw.js
+++ diet/sw.js
@@ -1,14 +1,17 @@
-const CACHE = 'diet-v1';
+const CACHE = 'godsaeng-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon-32.png'];
- 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
-);
-self.addEventListener('activate', e =>
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ))
-);
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting(); // 새 SW를 대기시키지 않고 바로 설치
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim()) // 열려있는 탭도 바로 새 SW가 장악
+  );
+});
 self.addEventListener('fetch', e =>
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
 );
